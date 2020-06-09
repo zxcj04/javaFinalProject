6bc4e60bfcb37a08cbbfd26847e67edd726bbd9f
ytchao0234 <nd68319@gamil.com>
2020-06-09 14:47:38 +0800

    remove spinner

--- JavaFinalGUI/src/javafinal/gui/MainFrameTest.java
+++ JavaFinalGUI/src/javafinal/gui/MainFrameTest.java
@@ -1,5 +1,7 @@
 package javafinal.gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 import fanrende.MainGee;
@@ -12,8 +14,9 @@ public class MainFrameTest
 		MainFrame mainFrame = new MainFrame(code, code.getList());
 		
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setSize(1000, 700);
-		mainFrame.setResizable(false);
+		mainFrame.setSize(1000, 705);
+		mainFrame.setMinimumSize(new Dimension(1000, 705));
+//		mainFrame.setResizable(false);
 		mainFrame.setLocationRelativeTo(null);
 		
 		LoadingFrame loading = new LoadingFrame();
